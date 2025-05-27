e8608e1553849a4480a8602af9920c6949e66c97
Gobyshanth <gobyg034@gmail.com>
2025-05-27 10:15:06 +0530

    error1 solve 7

--- server.js
+++ server.js
@@ -24,6 +24,8 @@ const gmailRoutes = require('./api/index');
 app.use('/', gmailRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
+
+module.exports = app;
